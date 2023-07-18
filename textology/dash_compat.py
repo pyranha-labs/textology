e85265867f7684cebe89aadfabfecaf74d3afdf3
David <dfrtzdev@gmail.com>
2023-07-18 17:01:01 -0600

    new: Add Dash compatibility layer for callbacks to events or attributes.

--- textology/dash_compat.py
+++ textology/dash_compat.py
@@ -1,13 +1,13 @@
 """Dash compatibility layer for creating Textual applications in "Plotly Dash" code style.
 
 The goal of this library is to provide a familiar interface for developers who have worked with Plotly Dash
-to make UI web applications, due to usage of similar principles. This can be used for basic applications,
-and to help transition developers to the style used in Textology for "observation pattern" type applications.
+to make UI web applications, due to usage of similar principles. This can be used for most applications,
+or to help transition developers to the style used in Textology for "observation pattern" type applications.
 
 While this library provides compatibility across frameworks in style where possible, it is not a guarantee or promise
 for any specific features or functionality. If a feature in Textology extended applications has an equivalent in Dash,
 it will be included here, but this is not Dash. In order to access the full range of Textology and Textual features,
-developers should leverage the native classes and designs from the standard library.
+developers may need to leverage the native classes and designs from the base libraries.
 """
 
 from typing import Callable
@@ -16,9 +16,15 @@ from textology.apps import ObservedApp
 
 # Compatibility aliases for Dash.
 from textology.observers import Dependency
-from textology.observers import Modified as Input  # pylint: disable=unused-import
+from textology.observers import Modified
+from textology.observers import NoUpdate  # pylint: disable=unused-import
+from textology.observers import Published
 from textology.observers import Select as State  # pylint: disable=unused-import
+from textology.observers import SupportsID
 from textology.observers import Update as Output  # pylint: disable=unused-import
+from textology.observers import when as callback  # pylint: disable=unused-import
+
+InputType = Modified | Published
 
 
 class DashCompatApp(ObservedApp):
@@ -55,3 +61,23 @@ class DashCompatApp(ObservedApp):
             self._observer_id_map,
             *args,
         )
+
+
+def Input(  # Treat this function as a class factory. pylint: disable=invalid-name
+    component_id: str | SupportsID,
+    property_or_event: str | type,
+) -> InputType:
+    """Initialize base dependency identification.
+
+    Args:
+        component_id: ID, or object with ID property, that a component uses to send and receive updates.
+        property_or_event: Property name, or event class type, on the component that triggers callbacks.
+
+    Return:
+        Reactive attribute dependency (Modified) if a string, message dependency (Published) otherwise.
+    """
+    if not isinstance(str, type):
+        raise ValueError("Input dependencies can only be strings, or classes")
+    if isinstance(property_or_event, str):
+        return Modified(component_id, property_or_event)
+    return Published(component_id, property_or_event)
