26219e7d1a7a95726bf99ea1f522cdfb88c8d3b5
David Fritz <dfrtzdev@gmail.com>
2024-02-18 14:43:47 -0700

    new: Add latest validation arguments to TextInput extension from Textual.

--- textology/widgets/_textual/_text_input.py
+++ textology/widgets/_textual/_text_input.py
@@ -8,9 +8,13 @@ from rich.highlighter import Highlighter
 from textual import events
 from textual import widgets
 from textual.suggester import Suggester
+from textual.types import InputValidationOn
 from textual.validation import Validator
+from typing_extensions import Literal
 
 from .._extensions import WidgetExtension
+
+InputType = Literal["integer", "number", "text"]
 
 
 class TextInput(WidgetExtension, widgets.Input):
@@ -19,15 +23,20 @@ class TextInput(WidgetExtension, widgets.Input):
     Renamed from "Input" to avoid conflicts with callback dependency types.
     """
 
-    def __init__(  # pylint: disable=too-many-arguments
+    def __init__(  # pylint: disable=too-many-arguments,too-many-locals
         self,
         value: str | None = None,
         placeholder: str = "",
         highlighter: Highlighter | None = None,
         password: bool = False,
         *,
+        restrict: str | None = None,
+        type: InputType = "text",
+        max_length: int = 0,
         suggester: Suggester | None = None,
         validators: Validator | Iterable[Validator] | None = None,
+        validate_on: Iterable[InputValidationOn] | None = None,
+        valid_empty: bool = False,
         name: str | None = None,
         id: str | None = None,
         classes: str | None = None,
@@ -43,8 +52,15 @@ class TextInput(WidgetExtension, widgets.Input):
             placeholder: Optional placeholder text for the input.
             highlighter: An optional highlighter for the input.
             password: Flag to say if the field should obfuscate its content.
+            restrict: A regex to restrict character inputs.
+            type: The type of the input.
+            max_length: The maximum length of the input, or 0 for no maximum length.
             suggester: Suggester associated with this input instance.
             validators: An iterable of validators that the Input value will be checked against.
+            validate_on: Zero or more of the values "blur", "changed", and "submitted",
+                which determine when to do input validation. The default is to do
+                validation for all messages.
+            valid_empty: Empty values are valid.
             name: Optional name for the input widget.
             id: Optional ID for the widget.
             classes: Optional initial classes for the widget.
@@ -58,8 +74,13 @@ class TextInput(WidgetExtension, widgets.Input):
             placeholder=placeholder,
             highlighter=highlighter,
             password=password,
+            restrict=restrict,
+            type=type,
+            max_length=max_length,
             suggester=suggester,
             validators=validators,
+            validate_on=validate_on,
+            valid_empty=valid_empty,
             name=name,
             id=id,
             classes=classes,
